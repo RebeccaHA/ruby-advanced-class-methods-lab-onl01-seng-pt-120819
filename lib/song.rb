2f391154d77ceeebc46d57e5d4b2a056510bef0b
calm-block-7153 <rebeccahunt034@gmail.com>
2020-01-04 12:11:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -58,7 +58,7 @@ class Song
     row = file_name
     data = row.split("-")
     artist_name = data[0]
-    data[1] = (name[1].chomp(".mp3"))
+    data[1] = (data[1].chomp(".mp3"))
     
     song = self.new
     song.name = song_name
